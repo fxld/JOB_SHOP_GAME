f13f2d5c86f26237a110bc73bfc37b5d4370bef9
ArCan314 <arcane314@qq.com>
2018-05-19 22:30:41 +0800

    变异

---
+++ mutant/main.c
@@ -0,0 +1,76 @@
+﻿#include"define.h"
+
+DATA **data;//输入数据data[Element][Machine]
+GENE island[ISLAND][MAXnum];//2个岛屿
+PROCESS ***Process;//使用四维数组存放解码出的设计图Process[island][gene][machine][工序]
+int after_select[ISLAND][MAXnum];//选择后基因的映射after_select[island][index of the selected],当after_selected[i][j]=-1时,表示已经进行过基因操作或是未选择
+int Element;//需加工的工件总数
+int Machine;//机器总数
+int Job;//总操作数，也就是基因的实际长度
+int age;//当前进化代数
+double Sum_fitness[2] = { 0 };//岛屿中所有个体适应度的和
+/***************************FOR DEBUG*************************************
+*	int mutanted[ISLAND][MAXnum];//表示经过突变的个体mutanted[island][index of the mutanted]
+*
+*/
+
+int main(void)
+{
+	/**************************
+	*	input();		//输入
+	*	initGen();		//生成初始种群
+	*	decode();		//解码
+	*	fitness();		//计算适应度
+	*	while(terminate())//是否达成跳出循环
+	*	{
+	*		sort();		//排序(依据适应度/运行时间)
+	*		select();	//选择
+	*		crossover();//交叉
+	*		mutant();	//变异
+	*		if(age%MOVEage==0)//判断是否迁移
+	*			move();	//迁移
+	*		decode();	//解码
+	*		fitness();	//计算适应度
+	*	}
+	*	output();		//输出
+	*	//animate();
+	*	return 0;
+	*/
+	input();//输入
+
+	code();//生成初始种群
+
+	for (int i = 0; i < ISLAND; i++)//解码计算适应度and排序
+	{
+		for (int j = 0; j < MAXnum; i++)
+		{
+			decode(&island[i][j]);//解码计算适应度
+			Sum_fitness[i] += island[i][j].fitness;
+		}
+		GENE *temporary = NULL;//临时空间
+		temporary = (GENE*)malloc(MAXnum * sizeof(GENE));
+		sort(0, MAXnum, island[i], temporary);//排序
+		free(temporary);
+	}
+	srand((unsigned int)time(NULL));
+	for (age = 0; age < MAXage; age++)//进化
+	{
+		//调用判断函数判断是否达成跳出循环的条件
+
+	}
+
+	if (island[0][0].makespan < island[1][0].makespan)//选择出当前最优解
+	{
+		decode(&island[0][0]);
+	}
+	else
+	{
+		decode(&island[1][0]);
+	}
+	/*此时Process中存放的是最优解的解析图*/
+	output();
+
+
+	system("pause");
+	return 0;
+}
---
+++ mutant/mutation.c
@@ -0,0 +1,52 @@
+﻿#include"define.h"
+
+int Get_Range(void)//获取一个区间[0,n],n∈(0,RAND_MAX),并且n/RAND_MAX=突变概率
+{
+	return (int)(RAND_MAX*MUTATION);
+}
+
+int cmp(const void *a, const void *b)
+{
+	return *(int*)b - *(int*)a;//当元素1-元素2大于0时降序
+}
+
+void qsort_after_select(void)//对after_select降序排序，使after_select[i][j]=-1排在后面
+{
+	for (int i = 0; i < ISLAND; i++)
+		qsort(after_select[i], MAXnum, sizeof(int), *cmp);
+}
+
+int mutant(void)//after_select[island][index of the selected]
+{
+	int interval;
+	int pos1, pos2, temp;//position
+	int i, j, k;
+
+	qsort_after_select();
+	j = after_select[i][0];
+	pos1 = pos2 = temp = 0;
+	interval = Get_Range();
+	
+	for (i=0;i<ISLAND;i++)
+		for(k=0;j!=-1;j=after_select[i][++k])
+			if (rand() <= interval)
+			{
+				pos1 = rand() % Job;
+				while ((pos2==pos1)||(island[i][j].gene[pos1]==island[i][j].gene[pos2]))
+					pos2 = rand() % Job;
+
+				temp = island[i][j].gene[pos1];
+				island[i][j].gene[pos1] = island[i][j].gene[pos2];
+				island[i][j].gene[pos2] = temp;
+				/***********FOR DEBUG***************
+				*	循环外：
+				*	int m=0;    
+				*	for (int i=0;i<ISLAND;i++)	
+				*		memset(mutanted[i],-1,sizeof(mutanted[i]));
+				*	循环内（加在注释上一行）：
+				*	mutanted[i][m++]=j;
+				*/
+			}
+}
+
+

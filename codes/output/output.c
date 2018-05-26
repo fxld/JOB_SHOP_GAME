9006bef7444a02f78e3627647de091bc96683056
994503782 <38678289+994503782@users.noreply.github.com>
2018-05-27 01:12:13 +0800

    output
    由于输出要求，我修改了PROCESS结构

---
+++ codes/output/output.c
@@ -0,0 +1,19 @@
+﻿#include"define.h"
+/*
+使用的全局变量：Element,Machine,Process
+*/
+void output(void)
+{
+	int i, j;
+	for (i = 0; i < Machine; i++)
+	{
+		printf("M%d", i);
+		for (j = 0; j < Element&&Process[i][j].component != -1; j++)
+		{
+			printf(" (%d,%d-%d,%d)", Process[i][j].start, Process[i][j].component, Process[i][j].operation, Process[i][j].end);
+		}
+		printf("\n");
+	}
+	printf("Time Used: %.3f\n",(double)clock()/1000);
+	printf("End Time: %d", BestMakeSpan);
+}
